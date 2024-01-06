cd120cd652c57dac4ceb7db4bd8a94ee6227cc90
Wahidur Rahman <wahidurrahman998@gmail.com>
2024-01-06 16:20:22 +0530

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-import http from 'http';
-import fs from 'fs';
-// import gfName from './feature.js';
-import {generateLovePercent}from './feature.js';
-const home=fs.readFileSync('./index.html','utf-8');
-console.log(home);
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+const app=express();
+app.set("view engine","ejs");
+// console.log();
+//using middle ware
+app.use(express.static(path.join(path.resolve(),"public")));
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
 
-const server=http.createServer((req,res)=>{
-  if(req.url=="/about"){
-    res.end(`<h1>Love is ${generateLovePercent()}</h1>`);
-  }else if(req.url=="/contact"){
-    res.end("<h1>Contact page</h1>");
-  }else if(req.url=="/"){
-    res.end("abc");
-  }else 
-    res.end("<h1>Page not found</h1>");
+mongoose.connect("mongodb://127.0.0.1:27017",{
+    dbName:"Whatsapp",
+}).then(()=>{
+    console.log("Databse Connected...")
+}).catch((e)=>console.log(e));
+const userSchema= new mongoose.Schema({
+    name:String,
+    email:String,
+});
+
+
+const User= new mongoose.model("User",userSchema);
+
+app.get("/",(req,res)=>{
+   res.render("login");
     
 });
-server.listen(5000,()=>{
-    console.log("server is listining");
+
+app.get("/logout",(req,res)=>{
+    res.cookie("token",null,{
+        httpOnly:true,
+        expires:new Date(Date.now())
+    });
+    res.redirect("/");
 })
+const isAuthenticated=(req,res)=>{
+    const {token}=req.cookies;
+    if(token){
+        next();
+    }else res.render("login");
+};
+app.get("/",isAuthenticated,(req,res)=>{
+    res.render("logout");
+})
+app.post("/login",(req,res)=>{
+res.cookie("token","iamin",{
+    httpOnly:true,
+    expires:new Date(Date.now()+60*1000),
+});
+res.redirect("/");
+});
+
+
+
+app.listen(5000,()=>console.log("server is working"));
